ee27e5a3cd1c586ffeae430f43c6e479dd9fe298
Kishan Kumar Das <kishankumar2672001@gmail.com>
2024-07-23 19:10:40 -0400

    changes in form

--- src/components/ImageComponent/ImageComponent.jsx
+++ src/components/ImageComponent/ImageComponent.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import styles from "./ImageComponent.module.css";
 
-const ImageComponent = ({src, alt, imageStyle}) => {
+const ImageComponent = ({src, alt, priority, imageStyle}) => {
   return (
 	<div className={imageStyle}>
 		<Image 
 			src={src}
 			alt={alt}
+			priority={priority}
 			className={styles.imageSizeStyle}
 		/>
 	</div>
