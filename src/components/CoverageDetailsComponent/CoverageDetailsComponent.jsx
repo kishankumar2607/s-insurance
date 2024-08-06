57616e926606c420daa962cd30b3032d4ebb9bec
Kishan Kumar Das <88204543+kishankumar2607@users.noreply.github.com>
2024-08-06 15:51:32 -0400

    Individual form Updated

--- src/components/CoverageDetailsComponent/CoverageDetailsComponent.jsx
+++ src/components/CoverageDetailsComponent/CoverageDetailsComponent.jsx
@@ -3,16 +3,16 @@ import CoverageDetailsCard from "./CoverageDetailsCard";
 import styles from "./CoverageDetailsComponent.module.css";
 
 const CoverageDetailsComponent = () => {
-  const checkboxLabels = ["Option 1", "Option 2", "Option 3", "Option 4"];
-  const addOnabels1 = ["Option 1", "Option 2", "Option 3", "Option 4"];
-  const addOnabels2 = ["Option 1", "Option 2", "Option 3", "Option 4"];
-  const addOnabels3 = ["Option 1", "Option 2", "Option 3", "Option 4"];
+  const checkboxLabels = ["Identify Theft Protection", "Data Restoration", "Business Continuity Planning", "Vulnerability Assessments"];
+  const addOnabels1 = ["Data Breaches", "Malware and Virus Attacks", "Phishing Attacks", "Cyber Extortion", "Incident Response Cost"];
+  const addOnabels2 = ["All Basic Coverage Inclusions", "Ransom Attacks", "Business Interruptions", "Legal Fees and Penalties", "Cyber Forensics"];
+  const addOnabels3 = ["All Basic and Enhanced Coverage Inclusions", "Reputation Management", "Crisis Mangement", "Regulatory Compilance COst", "Third Party Liability", "Employee Traning Programs"];
   return (
     <div className={styles.detailsMainDiv}>
-      <CoverageDetailsCard addOn addOnLabel={"title"} items={addOnabels1} />
-      <CoverageDetailsCard addOn addOnLabel={"title"} items={addOnabels2} />
-      <CoverageDetailsCard addOn addOnLabel={"title"} items={addOnabels3} />
-      <CoverageDetailsCard title={"title"} labels={checkboxLabels} />
+      <CoverageDetailsCard addOn addOnLabel={"Basic Coverage"} items={addOnabels1} />
+      <CoverageDetailsCard addOn addOnLabel={"Enhanced Coverage"} items={addOnabels2} />
+      <CoverageDetailsCard addOn addOnLabel={"Comprehensive Coverage"} items={addOnabels3} />
+      <CoverageDetailsCard title={"Add-Ons"} labels={checkboxLabels} />
     </div>
   );
 };
