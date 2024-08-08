8ad72e9e2e59905c49ceef7ed12831e61a55a81a
Kishan Kumar Das <88204543+kishankumar2607@users.noreply.github.com>
2024-08-08 12:34:54 -0400

    changed title

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,21 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./../src/components/Layout";
+import Head from 'next/head';
 
 import NextTopLoader from 'nextjs-toploader';
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <title>
+          SInsurance | Cyber Insurance, Life, Car, Travel, Home, Health
+          Insurance
+        </title>
+      </Head>
       <Layout>
-        <NextTopLoader 
+        <NextTopLoader
           color="#ec111a"
           showSpinner={false}
         />
